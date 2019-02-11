ce06eb977eaab1d80fa8fdbf23559cde6c7daddf
Marcel <fleuren.marcel@gmail.com>
2019-02-11 15:36:24 +0100

    refactored code and added module

--- week1/public/js/app.js
+++ week1/public/js/app.js
@@ -1,63 +1,40 @@
 "use strict"
+// from component folder
+import { planetDiv } from './components/planetDiv.js';
 
 let app = document.getElementById("root");
 let submit = document.getElementById("submit");
     submit.addEventListener("click", getData)
 
-
 function getData () {
-let pageNum = document.getElementById("pageNum").value;
-let url = `https://swapi.co/api/planets/?page=${pageNum}`;
-    console.log(pageNum)
+  let pageNum = document.getElementById("pageNum").value;
+  let url = `https://swapi.co/api/planets/?page=${pageNum}`;
 
- fetch(url)
-   .then(data => {
-     return data.json();
-   })
-   .then(res => {
+fetch(url)
+  .then(data => {
+    return data.json();
+  })
+  .then(res => {
     removePlanets()
     renderPlanets(res.results)
-   })
+  })
 
-  function renderPlanets(planets){
-    if (planets == 0){
-      noPlanets()
-      console.log("no planets")
-    } else {
-      console.log("fuck yeah, planets")
-      populatePlanets(planets)
-    }
+  let renderPlanets = planets => {
+    this == 0 ? noPlanets() : populatePlanets(planets)
   }
 
-  function populatePlanets (planets){
-    console.log(planets, "planetsding")
-    planets.forEach(planet => {
-      const planetContainer = document.createElement("section")
-      planetContainer.setAttribute("class", "planetContainer")
-      app.appendChild(planetContainer);
-
-      const planetTitle = document.createElement("h2")
-      planetTitle.textContent = planet.name;
-      planetContainer.appendChild(planetTitle)
-
-      const planetClimate = document.createElement("h3")
-      planetClimate.textContent = "climate: " + planet.climate
-      planetContainer.appendChild(planetClimate)
-
-      const planetTerrain = document.createElement("p")
-      planetTerrain.textContent = "Terrain: " + planet.terrain
-      planetContainer.appendChild(planetTerrain)
-      })
-    }
-}
-
-
-function removePlanets(){
-  let planetContainer = document.getElementsByClassName("planetContainer");
-  console.log(planetContainer)
-  for (var i = planetContainer.length - 1; i >= 0; i--) {
-    planetContainer[0].parentNode.removeChild(planetContainer[0]);
+  let populatePlanets = (planets) => {
+    let planetContainer = new planetDiv(planets);
+    planetContainer.render(app);
   }
+
+  let removePlanets = () => {
+      let planetContainer = document.getElementsByClassName("planetContainer");
+        for (let index of planetContainer) {
+        planetContainer[0].parentNode.removeChild(planetContainer[0]);
+     }
+    }
 }
 
 getData();
+export { app as app }
---
+++ week1/public/js/components/planetDiv.js
@@ -0,0 +1,31 @@
+import { app } from '../app.js'
+class planetDiv {
+
+    constructor(title, climate, terrain){
+        this.title = title;
+        this.climate = climate;
+        this.terrain = terrain;
+    }
+    render (){
+        this.title.forEach(planet => {
+            const planetContainer = document.createElement("section")
+            planetContainer.setAttribute("class", "planetContainer")
+            app.appendChild(planetContainer);
+
+            const planetTitle = document.createElement("h2")
+            planetTitle.textContent = planet.name;
+            planetContainer.appendChild(planetTitle)
+
+            const planetClimate = document.createElement("h3")
+            planetClimate.textContent = "climate: " + planet.climate
+            planetContainer.appendChild(planetClimate)
+
+            const planetTerrain = document.createElement("p")
+            planetTerrain.textContent = "Terrain: " + planet.terrain
+            planetContainer.appendChild(planetTerrain)
+        })
+    }
+
+}
+
+export { planetDiv };
